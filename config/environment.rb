5d6bcb1f94904590d491ebc0e6ae4c445f76c987
+ <+>
2022-08-29 10:13:16 +0300

    commit tree aabdf18fdd00c71c8bc04362a005854dc25c8e52

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,12 @@
-# Set up Bundler here following the instructions in the README
+# Set 
+require 'bundler/setup'
+Bundler.require(:default, :development)
+require 'rspec'
+require 'rest-client'
+require 'pry'
+array = [1,2,3,4]
+hash = {:a => 1, :b => 5}
+ap array
+ap hash
+
+
